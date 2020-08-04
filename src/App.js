85720de55ef755841f996822ac8a8aa7477b5c12
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-04 09:42:22 +0100

    Added get random Pokemon in array method.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import axios from "axios";
 import CardContainer from "./Components/Card_Container/CardContainer";
 import InputTotal from "./Components/Inputs/InputTotal";
+import PokemonGame from "./Components/Pokemon_Game/PokemonGame";
 
 class App extends React.Component {
   state = {
@@ -10,6 +11,28 @@ class App extends React.Component {
     picture: "",
     number: 0,
     name: "",
+    limit: 6,
+  };
+
+  // Get Random Pokemon in Array, limited by value in 'limit in state.
+  randomPokemon = () => {
+    // Create arrays
+    let pokeArray = [];
+    let randomNumber = [];
+    // Push 10 numbers to array
+    for (let i = 0; i < this.state.limit; i++) {
+      randomNumber.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    // API - Push Pokemon objects to Pokemon Array
+    randomNumber.map((number) =>
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${number}`, {
+          params: {},
+        })
+        .then((data) => pokeArray.push(data))
+        .then(() => this.setState({ pokemon: pokeArray }))
+    );
   };
 
   // Get Pokemon URL Array
@@ -30,9 +53,8 @@ class App extends React.Component {
           params: {},
         }
       );
-      console.log(response);
+
       this.setState({ pokemon: [response] });
-      // await this.loadingPokemon(response.data.results);
     }
   };
 
@@ -73,6 +95,11 @@ class App extends React.Component {
         {this.state.pokemon.length > 0 ? (
           <CardContainer cards={this.state.pokemon} />
         ) : null}
+        {/* Test Code */}
+        <button onClick={() => this.randomPokemon()}>
+          Random Pokemon Test Button
+        </button>
+        <PokemonGame pokemon={this.state.pokemon} />
       </div>
     );
   }
