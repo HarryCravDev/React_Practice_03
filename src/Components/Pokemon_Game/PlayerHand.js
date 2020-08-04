91898d25c81b5412343bfbe548534c9a21900b3b
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-04 16:14:37 +0100

    Started on game.

---
+++ src/Components/Pokemon_Game/PlayerHand.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlayerHand = () => {
+  return (
+    <div>
+      <h1>Player Hand</h1>
+    </div>
+  );
+};
+
+export default PlayerHand;
--- src/Components/Pokemon_Game/PokemonGame.js
+++ src/Components/Pokemon_Game/PokemonGame.js
@@ -1,13 +1,44 @@
 import React from "react";
+import PlayerHand from "./PlayerHand";
 
-class PokemonGame extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Pokemon Game</h1>
-      </div>
-    );
+const PokemonGame = ({ pokemon }) => {
+  if (pokemon.length !== 0) {
+    // Player Hands
+    const hand1 = [...pokemon];
+    let hand2 = [];
+
+    // Split Pokemon between Hand1 & Hand2
+    for (let i = 0; i < 3; i++) {
+      let pokemon = hand1.splice(i, 1)[0];
+      hand2.push(pokemon);
+    }
+
+    // Calculate Experience
+    let p1;
+    let p2;
+    console.log(hand2.length);
+    if (hand1.length === 3 && hand2.length === 3) {
+      p1 = hand1.reduce(
+        (exp, pokemon) => exp + pokemon.data.base_experience,
+        0
+      );
+
+      p2 = hand2.reduce(
+        (exp, pokemon) => exp + pokemon.data.base_experience,
+        0
+      );
+
+      console.log(p1);
+      console.log(p2);
+    }
   }
-}
+  console.log("Working!");
+
+  return (
+    <div>
+      <h1>Hello there</h1>
+    </div>
+  );
+};
 
 export default PokemonGame;
